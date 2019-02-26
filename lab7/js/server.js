f0f4c555941cf5a99cf6219966ecb4ede7c1f4df
Viktor Malych <victormalych@gmail.com>
2019-02-26 19:39:07 +0000

    lab7 done

--- lab7/js/server.js
+++ lab7/js/server.js
@@ -1,5 +1,40 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-var app = connect();
-app.use(serveStatic(__dirname));
-app.listen(8080, function(){console.log("Server running on 8080..."));
+const { createServer } = require("http");
+
+const PORT = process.env.PORT || 5000;
+
+const server = createServer();
+
+server.on("request", (request, response) => 
+{ 
+	if (request.method === "POST")
+	{
+		let data = "";
+		request.on("data", chunk =>
+		{
+			data += chunk;
+		});
+		
+		request.on("end", () =>
+		{
+			try
+			{
+				const requestData = JSON.parse(data);
+				requestData.ourMessage = "success";
+				response.setHeader("Content-Type", "application/json");
+				response.end(JSON.stringify(requestData));
+			}
+			catch (e) 
+			{
+				response.statusCode = 400;
+				response.end("Invalid JSON");
+			}
+		});
+	}
+	else
+	{
+		response.statusCode = 400;
+		response.end("Please POST a JSON object");
+	}
+});
+
+server.listen(PORT, () => { console.log(`starting server at port ${PORT}`); });
