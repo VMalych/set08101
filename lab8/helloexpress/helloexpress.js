2b9e77f78b9ae3d3679b41859da2a6082e8327c7
Viktor Malych <victormalych@gmail.com>
2019-03-05 16:29:49 +0000

    lab 8  complete

--- lab8/helloexpress/helloexpress.js
+++ lab8/helloexpress/helloexpress.js
@@ -3,17 +3,20 @@ var app = express();
 
 app.get("/", function (req, res) 
 {
-	res.send("A GET request to the root resource");
+	res.set("Content-Type", "text/html");
+	res.send(new Buffer("<h1>The root route</h1>"));
 });
 
-app.post("/", function (req, res) 
+app.get("/hello", function (req, res) 
 {
-	res.send("A POST request to the root resource");
+	res.set("Content-Type", "text/html");
+	res.send(new Buffer("<h2>The hello route</h2>"));
 });
 
-app.head("/", function (req, res) 
+app.get("/goodbye", function (req, res) 
 {
-	res.send("A HEAD request to the root resource");
+	res.set("Content-Type", "text/html");
+	res.send(new Buffer("<p>The <b>goodbye</b> route</p>"));
 });
 
 var server = app.listen(5000, "127.0.0.1", function()
