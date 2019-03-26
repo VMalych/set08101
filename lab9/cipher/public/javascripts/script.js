fa476952d7a333b57a8c6f4d5c707bf039e8f124
Viktor Malych <victormalych@gmail.com>
2019-03-26 16:53:32 +0000

    lab 10 progress

--- lab9/cipher/public/javascripts/script.js
+++ lab9/cipher/public/javascripts/script.js
@@ -155,3 +155,51 @@ function d_base64(input)
 	output = atob(input);
 	document.getElementById("output").innerHTML	= output;
 }
+
+function getCookie(cookie_name)
+{
+	var name = cookie_name + "=";
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(";");
+	for (var i = 0; i < ca.length; i++)
+	{
+		var c = ca[i];
+		while (c.charAt(0) == " ")
+		{
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0)
+		{
+			return c.substring(name.length, c .length);
+		}
+	}
+	return "";
+}
+
+function setCookie(name, value, expiry)
+{
+	var d = new Date();
+	d.setTime(d.getTime() + (expiry*24*60*60*1000));
+	var expires = "expires=" + d.toUTCString();
+	document.cookie = name + "=" + value + ";" + expires;
+}
+
+function storageAvailable(type)
+{
+	try
+	{
+		var storage = window[type], x = "__storage_test__";
+		storage.setItem(x, x);
+		storage.removeItem(x);
+		return true;
+	}
+	catch(e)
+	{
+		return e instanceof DOMException && (
+			e.code === 22 ||
+			e.code === 1014 ||
+			e.name === "QuotaExceededError" ||
+			e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+			storage.length !== 0;
+	}
+}
--- lab9/cipher/routes/index.js
+++ lab9/cipher/routes/index.js
@@ -1,5 +1,35 @@
+var path = require("path");
+var dbPath = path.resolve(__dirname, "count.db");
+
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database(dbPath);
+
+db.serialize(function() {
+	db.run("CREATE TABLE IF NOT EXISTS counts (key TEXT, value INTEGER)");
+	db.run("INSERT INTO counts (key, value) VALUES (?, ?)", "counter", 0);
+});
+
 var express = require('express');
 var router = express.Router();
+
+router.get("/data", function(req, res) {
+  db.get("SELECT value FROM counts", function(err, row) {
+    res.json({"count" : row.value});
+  });
+});
+
+router.get("/data", function(req, res) {
+  db.run("UPDATE counts SET value = value + 1 WHERE key = ?", "counter", function(err, row) {
+    if (err) {
+      console.err(err);
+      res.status(500);
+    }
+	else {
+      res.status(202);
+    }
+    res.end();
+  });
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
