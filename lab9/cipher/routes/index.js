8acf6ca2d614a4d0db9da62eb67318130ec111f1
Viktor Malych <victormalych@gmail.com>
2019-03-30 13:32:32 +0000

    lab11

--- lab9/cipher/routes/index.js
+++ lab9/cipher/routes/index.js
@@ -1,5 +1,20 @@
+'use strict';
 var express = require('express');
 var router = express.Router();
+
+const winston = require('winston');
+const { createLogger, format, transports } = require('winston');
+const logger = winston.createLogger({
+	level: 'debug',
+	format: format.combine(
+			format.timestamp(),
+			format.json()
+		),
+	trasnports: [new transports.File({
+		filename: 'app.log',
+		'timestamp': true
+	})]
+});
 
 const MongoClient = require("mongodb").MongoClient;
 
@@ -27,9 +42,46 @@ MongoClient.connect(MONGO_URL, (err, client) => {
   )
 });
 
+const bcrypt = require('bcrypt');
+
+var userpw = "kappa";
+var hash = bcrypt.hashSync(userpw, 10);
+console.log("Hashed password: " + hash);
+
+console.log("Using password: '" + userpw + "' we get: ");
+if (bcrypt.compareSync(userpw, hash))
+{
+	logger.log("info", "match");
+}
+else
+{
+	logger.log("info", "no match");
+}
+
+userpw = "";
+
+function isAuthenticated(req, res, next)
+{
+	userpw = req.query.password || "";
+	console.log("Authenticating: " + (req.query.username || "no one"));
+	
+	if (bcrypt.compareSync(userpw, hash))
+	{
+		logger.log("info", "Authorization success");
+		return next();
+	}
+	
+	logger.warn("Authorization fail");
+	res.redirect('/login');
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', isAuthenticated, function(req, res, next) {
   res.render('index', { title: 'Cipher' });
+});
+
+router.get('/login', function(req, res, next) {
+	res.render('login');
 });
 
 router.get('/caesar/encrypted', function(req, res, next) {
@@ -42,7 +94,7 @@ router.get('/cotr/encrypted', function(req, res, next) {
   res.render('cipher', { title: 'Cotr', message: message});
 });
 
-router.get('/base64/encrypted', function(req, res, next) {
+router.get('/base64/encrypted', isAuthenticated, function(req, res, next) {
   var message = req.query.message;
   res.render('cipher', { title: 'Base64', message: message});
 });
@@ -57,7 +109,7 @@ router.get('/cotr/decrypted', function(req, res, next) {
   res.render('decipher', { title: 'Cotr', message: message});
 });
 
-router.get('/base64/decrypted', function(req, res, next) {
+router.get('/base64/decrypted', isAuthenticated, function(req, res, next) {
   var message = req.query.message;
   res.render('decipher', { title: 'Base64', message: message});
 });
